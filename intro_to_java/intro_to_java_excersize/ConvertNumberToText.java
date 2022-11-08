572efeef9b37742bc92190df4e74c2ac12ef3768
quynhngo2909 <dieuquynh105@gmail.com>
2022-11-08 14:06:34 +0700

    nop bt

--- intro_to_java/intro_to_java_excersize/ConvertNumberToText.java
+++ intro_to_java/intro_to_java_excersize/ConvertNumberToText.java
@@ -14,7 +14,7 @@ public class ConvertNumberToText {
         if (weight > 999 || weight < 0) {
             numberInText += "Out of ability";
         }
-        if (weight ==  0) {
+        if (weight == 0) {
             numberInText += "Zero";
         }
         switch (hundreds) {
@@ -46,96 +46,190 @@ public class ConvertNumberToText {
                 numberInText += "nine hundred";
                 break;
         }
-        switch (dozens) {
-            case 0:
-                numberInText += " and";
-                break;
-            case 2:
-                numberInText += " and twenty";
-                break;
-            case 3:
-                numberInText += " and thirty";
-                break;
-            case 4:
-                numberInText += " and fourty";
-                break;
-            case 5:
-                numberInText += " and fifty";
-                break;
-            case 6:
-                numberInText += " and sixty";
-                break;
-            case 7:
-                numberInText += " and seventy";
-                break;
-            case 8:
-                numberInText += " and eighty";
-                break;
-            case 9:
-                numberInText += " and ninety";
-                break;
-        }
-        if (dozens == 1) {
-            switch (units) {
-                case 1:
-                    numberInText += " and eleven";
-                    break;
+        if (hundreds != 0) {
+            switch (dozens) {
                 case 2:
-                    numberInText += " and twelve";
+                    numberInText += " and twenty";
                     break;
                 case 3:
-                    numberInText += " and thirteen";
+                    numberInText += " and thirty";
                     break;
                 case 4:
-                    numberInText += " and fourteen";
+                    numberInText += " and forty";
                     break;
                 case 5:
-                    numberInText += " and fifteen";
+                    numberInText += " and fifty";
                     break;
                 case 6:
-                    numberInText += " and sixteen";
+                    numberInText += " and sixty";
                     break;
                 case 7:
-                    numberInText += " and seventeen";
+                    numberInText += " and seventy";
                     break;
                 case 8:
-                    numberInText += " and eighteen";
+                    numberInText += " and eighty";
                     break;
                 case 9:
-                    numberInText += " and nineteen";
+                    numberInText += " and ninety";
                     break;
+            }
+            if (dozens == 1) {
+                switch (units) {
+                    case 0:
+                        numberInText += " and ten";
+                        break;
+                    case 1:
+                        numberInText += " and eleven";
+                        break;
+                    case 2:
+                        numberInText += " and twelve";
+                        break;
+                    case 3:
+                        numberInText += " and thirteen";
+                        break;
+                    case 4:
+                        numberInText += " and fourteen";
+                        break;
+                    case 5:
+                        numberInText += " and fifteen";
+                        break;
+                    case 6:
+                        numberInText += " and sixteen";
+                        break;
+                    case 7:
+                        numberInText += " and seventeen";
+                        break;
+                    case 8:
+                        numberInText += " and eighteen";
+                        break;
+                    case 9:
+                        numberInText += " and nineteen";
+                        break;
+                }
+            } else {
+                switch (units) {
+                    case 1:
+                        numberInText += " and one";
+                        break;
+                    case 2:
+                        numberInText += " and two";
+                        break;
+                    case 3:
+                        numberInText += " and three";
+                        break;
+                    case 4:
+                        numberInText += " and four";
+                        break;
+                    case 5:
+                        numberInText += " and five";
+                        break;
+                    case 6:
+                        numberInText += " and six";
+                        break;
+                    case 7:
+                        numberInText += " and seven";
+                        break;
+                    case 8:
+                        numberInText += " and eight";
+                        break;
+                    case 9:
+                        numberInText += " and nine";
+                        break;
+                }
             }
         } else {
-            switch (units) {
-                case 1:
-                    numberInText += " one";
-                    break;
+            switch (dozens) {
                 case 2:
-                    numberInText += " two";
+                    numberInText += " twenty";
                     break;
                 case 3:
-                    numberInText += " three";
+                    numberInText += " thirty";
                     break;
                 case 4:
-                    numberInText += " four";
+                    numberInText += " forty";
                     break;
                 case 5:
-                    numberInText += " five";
+                    numberInText += " fifty";
                     break;
                 case 6:
-                    numberInText += " six";
+                    numberInText += " sixty";
                     break;
                 case 7:
-                    numberInText += " seven";
+                    numberInText += " seventy";
                     break;
                 case 8:
-                    numberInText += " eight";
+                    numberInText += " eighty";
                     break;
                 case 9:
-                    numberInText += " nine";
+                    numberInText += " ninety";
                     break;
             }
-        }
+            if (dozens == 1) {
+                switch (units) {
+                    case 0:
+                        numberInText += " ten";
+                        break;
+                    case 1:
+                        numberInText += " eleven";
+                        break;
+                    case 2:
+                        numberInText += " twelve";
+                        break;
+                    case 3:
+                        numberInText += " thirteen";
+                        break;
+                    case 4:
+                        numberInText += " fourteen";
+                        break;
+                    case 5:
+                        numberInText += " fifteen";
+                        break;
+                    case 6:
+                        numberInText += " sixteen";
+                        break;
+                    case 7:
+                        numberInText += " and seventeen";
+                        break;
+                    case 8:
+                        numberInText += " eighteen";
+                        break;
+                    case 9:
+                        numberInText += "nineteen";
+                        break;
+                }
+            }
+                else {
+                    switch (units) {
+                        case 1:
+                            numberInText += " one";
+                            break;
+                        case 2:
+                            numberInText += " two";
+                            break;
+                        case 3:
+                            numberInText += " three";
+                            break;
+                        case 4:
+                            numberInText += " four";
+                            break;
+                        case 5:
+                            numberInText += " five";
+                            break;
+                        case 6:
+                            numberInText += " six";
+                            break;
+                        case 7:
+                            numberInText += " seven";
+                            break;
+                        case 8:
+                            numberInText += " eight";
+                            break;
+                        case 9:
+                            numberInText += " nine";
+                            break;
+                    }
+                }
+            }
         System.out.println(numberInText);
-    }
+}
 }
