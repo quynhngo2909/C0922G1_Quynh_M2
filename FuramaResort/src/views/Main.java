a68c7a064dec4a93132e975fba8eac9a695e1568
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-05 16:58:59 +0700

    nop bt

---
+++ FuramaResort/src/views/Main.java
@@ -0,0 +1,10 @@
+package views;
+
+import controllers.FuramaController;
+
+public class Main {
+    public static void main(String[] args) {
+        FuramaController furamaController = new FuramaController();
+        furamaController.displayMainMenu();
+    }
+}
